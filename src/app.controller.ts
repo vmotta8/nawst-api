bfe8e35740a697d89c9593a4f2bbcd6dcd9dc3c5
Vinicius Motta <viniciusmotta8@gmail.com>
2021-12-21 23:28:14 -0300

    simple routes created

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/user')
+  postUser(): string {
+    return this.appService.postUser();
+  }
+
+  @Get('/user')
+  getUsers(): string {
+    return this.appService.getUsers();
+  }
+
+  @Get('/receipts')
+  getReceipts(): string {
+    return this.appService.getReceipts();
+  }
+
+  @Post('/payment')
+  postPayment(): string {
+    return this.appService.postPayment();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,25 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  postUser() {
+    return 'User created!'
+  }
+
+  getUsers() {
+    return 'All users!'
+  }
+
+  getReceipts() {
+    return 'All receipts!'
+  }
+
+  postPayment() {
+    this.addToPaymentQueue()
+    return 'Post payment!'
+  }
+
+  private addToPaymentQueue() {
+    console.log('Added to the queue!')
+  }
 }
