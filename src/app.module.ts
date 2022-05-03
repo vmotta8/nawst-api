930703c82507d2489e378c677d235bee4ded8e48
Vinicius Motta - Yuca <vinicius.motta@yuca.live>
2022-05-02 21:52:17 -0300

    dotenv added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseConfig } from './config';
 import { ProductsModule } from './modules/products/products.module';
 
 const DatabaseModule = SequelizeModule.forRoot({
-  dialect: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'root',
-  password: 'root',
-  database: 'simple_marketplace',
+  ...DatabaseConfig,
   autoLoadModels: true,
   synchronize: true,
   logging: false,
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
+import { Dialect } from 'sequelize/types';
 
-module.exports = {
-  dialect: process.env.DB_DIALECT,
+export const DatabaseConfig = {
+  dialect: process.env.DB_DIALECT as Dialect,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: +process.env.DB_PORT || 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
